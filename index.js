bed260d0a65f5c4fb08c5457489be789c3ade774
alejandro21-rock <alejandrodeangelis21@gmail.com>
2025-03-15 15:50:11 -0300

    install cors

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
-
+const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 morgan.token("body", (req) => JSON.stringify(req.body));
 app.use(morgan("tiny"));
 
@@ -102,5 +102,4 @@ app.post(
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-
 });

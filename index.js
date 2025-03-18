7f93d2df9dd35cebaf4e720f299569305db547f7
alejandro21-rock <alejandrodeangelis21@gmail.com>
2025-03-18 12:59:04 -0300

    express dist

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static("dist"));
 morgan.token("body", (req) => JSON.stringify(req.body));
 app.use(morgan("tiny"));
 
